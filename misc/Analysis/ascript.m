e9a0466be546b8dbbddd24c36c4b510e6d74f858
David Martin <DavidMartin214@hotmail.co.uk>
2017-03-17 00:26:14 +0000

    Presentation
    Presentation draft and some figures

---
+++ misc/Analysis/ascript.m
@@ -0,0 +1,10 @@
+results= [2   3   3   3   4   2   2   3   4   4   2   3   2   4   3   3   2   3   4   3   4   2   3   4 2 3 3 3 2 3 2 3 3 5 2 3 2 3 3 3 4 4 5];
+bins = [2 3 4 5 6 7 8 9 10];
+hist(results);
+xlabel("Victory Points");
+ylabel("Frequency");
+numResults = length(results);
+axis([2 10 0 numResults/2]);
+title("Results of random play");
+avg = mean(results);
+dev = std(results);
